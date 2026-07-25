f4a27ee93065ddfdfe3a584090066bfa64296b3c
bogdan-chis <chisbogdan.2020cbm@gmail.com>
2026-07-25 10:51:05 +0300

    Created interface for Outfit repository

---
+++ backend/ClosetCompanionApp/ClosetCompanionApp/Repository/IOutfitRepository.cs
@@ -0,0 +1,13 @@
+﻿using ClosetCompanionApp.Domain;
+
+namespace ClosetCompanionApp.Repository
+{
+    public interface IOutfitRepository
+    {
+        Task AddAsync(GeneratedOutfit outfit);
+        Task DeleteAsync(Guid id);
+        Task<GeneratedOutfit> GetByIdAsync(Guid id);
+        Task<IEnumerable<GeneratedOutfit>> GetFavoritesAsync();
+        Task UpdateAsync(GeneratedOutfit outfit);
+    }
+}
--- backend/ClosetCompanionApp/ClosetCompanionApp/obj/Debug/net8.0/ClosetCompanionApp.AssemblyInfo.cs
+++ backend/ClosetCompanionApp/ClosetCompanionApp/obj/Debug/net8.0/ClosetCompanionApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClosetCompanionApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd7067393e28c97e09c264d9301ce7e666ede6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa00db01a3fdcc46aeaed3d07b1bf20267f1fb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClosetCompanionApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClosetCompanionApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
